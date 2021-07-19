b56c12975c0112c1ddfb9cf45fe707866d08fe1e
harsidhkamat <73664427+harsidhkamat@users.noreply.github.com>
2021-07-19 12:00:20 +0100

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,25 @@
+class Paper{
+    constructor(){
+        var options={
+            isStatic:false,
+            restitution:0.3, 
+            friction:0.5,
+            density:1.2,
+
+            
+        }
+        this.body=Bodies.circle(100,200,20,options);
+        World.add(world,this.body)
+
+    }
+    display(){
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        angleMode(RADIANS);
+        rotate(this.body.angle);
+        ellipseMode(RADIUS);
+        fill("purple")
+        ellipse(0,0,17,17);
+        pop();
+    }
+}
